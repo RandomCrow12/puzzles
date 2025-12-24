05057498775936cee91c5e74bfdd6a88013168b1
RandomCrow12 <123201070+RandomCrow12@users.noreply.github.com>
2025-12-23 19:34:49 -0600

    snip-example.cpp

---
+++ Leetcode/snip-example.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+//#include <map>
+//#include <set>
+//#include <unordered_map>
+//#include <unordered_set>
+//#include<bits/stdc++.h>
+#define ll long long
+//using namespace std;
+
+
+int main(){
+
+
+    return 0;
+}
+
+
+// I like the var_name convention over varName...
+// easier to read imo but sometimes I'll have to use camel case lmao
